66acab196de55f568384db820860dcb43946f16f
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2025-05-16 12:32:54 +0200

    words

--- endgame/src/App.jsx
+++ endgame/src/App.jsx
@@ -2,8 +2,11 @@ import React from "react";
 import { languages } from "./languajes";
 
 export default function AssemblyEndgame() {
+  const [currentWord, setCurrentWord] = React.useState("react");
+
   const languageElements = languages.map((language) => (
-    <div className="language-container"
+    <div
+      className="language-container"
       key={language.name}
       style={{
         backgroundColor: language.backgroundColor,
@@ -12,6 +15,12 @@ export default function AssemblyEndgame() {
     >
       {language.name}
     </div>
+  ));
+
+  const letterSpans = currentWord.split("").map((letter, index) => (
+    <span key={index} className="letter-span">
+      {letter}
+    </span>
   ));
 
   return (
@@ -28,6 +37,7 @@ export default function AssemblyEndgame() {
         <p>Good game!</p>
       </section>
       <section className="languages">{languageElements}</section>
+      <section className="word-display">{letterSpans}</section>
     </main>
   );
 }
